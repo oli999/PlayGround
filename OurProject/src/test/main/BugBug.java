4f2121f3bebd0223e00ed1251eee1b829cccc645
sungwon <zhtmr123@gmail.com>
2019-06-03 18:06:11 +0900

    벅

--- OurProject/src/test/main/BugBug.java
+++ OurProject/src/test/main/BugBug.java
@@ -4,10 +4,11 @@ public class BugBug {
 	
 	private String bug="오류류르르르르르르";
 	private String bug2="꺄르르르릉";
-	
+	private String bug3="누가 벅벅소리를?";
 	public void bug(){
 		System.out.println(bug);
 		System.out.println(bug2);
+		System.out.println(bug3);
 	}
 	
 }
