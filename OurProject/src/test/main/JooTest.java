99e6f4c3b30307a480f3c7ab0bb772274feafb38
Joo <eyebabyuniverse@gmail.com>
2019-06-03 18:02:34 +0900

    commit from Joo

---
+++ OurProject/src/test/main/JooTest.java
@@ -0,0 +1,23 @@
+package test.main;
+
+public class JooTest {
+	private String name;
+	
+	//생성자
+	public JooTest(){}
+	
+	public JooTest(String name) {
+		super();
+		this.name = name;
+	}
+
+	//getter&setter method
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
