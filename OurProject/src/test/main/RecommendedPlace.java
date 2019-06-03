9bbc2a0f86072fe8e5821c03163eb789aa2b7ccc
Dend <dendmono@gmail.com>
2019-06-03 18:03:53 +0900

    Dend added 'RecommendedPlace.java' for no reason

--- OurProject/src/test/main/RecommendedPlace.java
+++ OurProject/src/test/main/RecommendedPlace.java
@@ -2,7 +2,7 @@ package test.main;
 
 public class RecommendedPlace {
 	
-	public String recommend="을지로 주변 술집 싸고 맛있는데 아래 변수로 추천 좀...";
+	public String recommend="을지로 주변 술집 싸고 맛있는데 아래 변수로 추천 좀...z";
 	
 	
 }
